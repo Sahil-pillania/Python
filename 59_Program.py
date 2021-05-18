30f01f7ee558864b27f4036a93541ffbe81c3285
Sahil pillania <80634110+Sahil-pillania@users.noreply.github.com>
2021-05-18 20:23:22 +0530

    Updated

--- 59_Program.py
+++ 59_Program.py
@@ -1,4 +1,4 @@
-#Program to greet all the person names strored in a list l1 and which starts with 5
+#Program to greet all the person names stored in a list l1 and which starts with "S".
 l1 = ["Harry", "Sohan", "Sahil", "Rahul"]
 
 for name in l1:
