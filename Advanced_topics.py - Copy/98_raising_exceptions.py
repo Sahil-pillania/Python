6b2e8cfa042906bd2bb219fe93536e1970ba5b5a
Sahil-pillania <sahilpillania88@gmail.com>
2022-04-16 14:46:17 +0530

    Programs

--- Advanced_topics.py - Copy/98_raising_exceptions.py
+++ Advanced_topics.py - Copy/98_raising_exceptions.py
@@ -2,7 +2,8 @@ def increment(num):
     try:
         return int(num) + 1
     except:
-        raise ValueError("This is not good ")
+        raise ValueError("There is an error.")
+
 
 a = increment('df364')
 print(a)
